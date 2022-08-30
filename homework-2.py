3abce2a8db2873a7595878772e5be6ac90add6e8
MCatapang <maocatap@gmail.com>
2022-08-29 21:40:49 -0700

    Formatted float_div to 2 decimal places

--- homework-2.py
+++ homework-2.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter an integer: "))
 num2 = int(input("Enter another integer: "))
 
 product = num1 * num2
-float_div = num1 / num2
+float_div = float(format((num1 / num2), '.2f'))
 int_div = num1 // num2
 remainder = num1 % num2
 sum = num1 + num2
@@ -10,7 +10,7 @@ diff = num1 - num2
 
 print(
     f"{num1} * {num2} = {product}",
-    f"\n{num1} / {num2} = {float_div}",
+    f"\n{num1} / {num2} = {float_div}", 
     f"\n{num1} // {num2} = {int_div}",
     f"\n{num1} % {num2} = {remainder}",
     f"\n{num1} + {num2} = {sum}",
